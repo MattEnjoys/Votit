5b54f25d5660288913fd655c28708173a42eb8ab
MattEnjoys <matthieu.ebzant@orange.fr>
2023-11-26 16:34:27 +0100

    Live 2.

--- README.md
+++ README.md
@@ -6,8 +6,19 @@ Projet gité sur [le git d'Anthony Laplane](https://github.com/arirangz/studi_vo
 
 ## Live 1 :
 
-> Mise en place des bases: Récupérations des assets, fournis par le live ainsi que la BDD au format SQL.
-> Création du MCD.
-> Initiation de la page index.php avec Bootstrap.
-> Création d'un domaine local, comme si on avais un nom de domaine. (Voir le PDF du projet.)
-> /|\ A partir de maintenant il faut [tapper](http://votit.local/) pour aller sur le site en développement.
+> Mise en place des bases: Je récupére les assets, fournis par le live, ainsi que la BDD au format SQL.
+> Création du MCD dans le live.
+> J'initialise la page index.php avec [Bootstrap](https://getbootstrap.com/).
+> Je crée un domaine local, comme si on avais un nom de domaine. (Voir le PDF du projet.)
+> /|\ A partir de maintenant je tappe [votit.local](http://votit.local/) pour aller sur le site en développement.
+
+## Live 2 :
+
+> Je crée l'ossature de index.php avec Bootstrap.
+> Je crée le dossier template afin de couper le header et le footer dans des fichiers appellés de même nom, et je require_once les fichiers.
+> Dans ce dossier template, je crée poll_part.php afin de centraliser les card Bootstrap que je modifie en fonction de la demande.
+> Je crée le dossier lib afin de faire le fichier config.php pour y mettre les constantes de base que j'inclue dans header.php.
+> Je récupère les enregistrements de la BDD, en mettant en place PDO et en définissant les constantes et j'appelle pdo.php dans le header.
+> Je crée poll.php pour inscrire la requête pour récupérer les sondages pour les afficher sur index.php.
+> Sur index.php, je boucle sur poll.php.
+> Je rends poll_part.php dynamique.
--- index.php
+++ index.php
@@ -1,24 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <h1>TEST</h1>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-            crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php require_once 'templates/header.php';
+require_once 'lib/poll.php';
+$polls = getPolls($pdo);
+?>
+<!-- Content -->
+<!-- Heros -->
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div class="col-10 col-sm-8 col-lg-6">
+        <img src="<?= PATH_ASSETS_IMAGES ?>logo-votit.png"
+             class="d-block mx-lg-auto img-fluid"
+             alt="Logo"
+             width="400"
+             height="500"
+             loading="lazy">
+    </div>
+    <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Votez sur des sujets de l'actualité IT</h1>
+        <p class="lead">VotIt : Là où la communauté tech s'exprime. De la préférence entre frameworks front-end aux
+            débats sur les meilleures pratiques DevOps, nous vous offrons une plateforme pour créer, participer et
+            analyser des sondages spécifiquement centrés sur le monde du développement, de l'IT et du DevOps.
+            Rejoignez-nous, partagez vos opinions et restez à jour avec les tendances de notre industrie !</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <a href="#"
+               type="button"
+               class="btn btn-primary btn-lg px-4 me-md-2">Voir tous les sondages</a>
+        </div>
+    </div>
+</div>
+<!-- Fin Heros -->
+<div class="row text-center">
+    <h2>Les derniers sondages :</h2>
+    <?php foreach ($polls as $poll) {
+        require 'templates/poll_part.php';
+    } ?>
+</div>
+<!-- Fin Content -->
+<?php require_once 'templates/footer.php'; ?>
